21a70beccd9418fea65037e54bbcad7242c14ca7
Jonas Rønning <jr.ronning@gmail.com>
2023-09-17 20:09:52 +0200

    More presentable store page

--- storeway/src/Pages/StorePage.tsx
+++ storeway/src/Pages/StorePage.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Store } from '../lib/types'
 import { useNavigate } from 'react-router-dom'
+import StandardButton from '../Components/Button/StandardButton'
+import './StorePage.css'
 import {
 	QueryClient,
 	QueryClientProvider,
@@ -78,14 +80,80 @@ function GetStore() {
 	if (isLoading) return <div>Loading...</div>
 
 	return (
-		<>
-			<div> {storeID} </div>
-			<div> {store?.address} </div>
-			<div> {store?.name} </div>
-			<div> {store?.position.lat} </div>
-			<div> {store?.position.lng} </div>
-			<button onClick={() => handleStoreChange(false)}> Forrige Butikk </button>
-			<button onClick={() => handleStoreChange(true)}> Neste Butikk</button>
-		</>
+		<div className="container">
+			<StandardButton
+				text={'←'}
+				state={false}
+				handleClick={() => handleStoreChange(false)}
+			/>
+			<div className="information-container">
+				<div className="logo-name">
+					<img src={store?.logo}></img>
+					<h2> {store?.name} </h2>
+				</div>
+				<div className="company-info">
+					<p> {store?.address} </p>
+					<p> Telefon: {store?.phone} </p>
+					<h3> Åpningstider: </h3>
+					<div className="opening-hours">
+						<p>
+							{' '}
+							Mandag:{' '}
+							{store?.openingHours.monday
+								? store.openingHours.monday
+								: 'Stengt'}
+						</p>
+						<p>
+							{' '}
+							Tirsdag:{' '}
+							{store?.openingHours.tuesday
+								? store.openingHours.tuesday
+								: 'Stengt'}
+						</p>
+						<p>
+							{' '}
+							Onsdag:{' '}
+							{store?.openingHours.wednesday
+								? store.openingHours.wednesday
+								: 'Stengt'}
+						</p>
+						<p>
+							{' '}
+							Torsdag:{' '}
+							{store?.openingHours.thursday
+								? store.openingHours.thursday
+								: 'Stengt'}
+						</p>
+						<p>
+							{' '}
+							Fredag:{' '}
+							{store?.openingHours.friday
+								? store.openingHours.friday
+								: 'Stengt'}
+						</p>
+						<p>
+							{' '}
+							Lørdag:{' '}
+							{store?.openingHours.saturday
+								? store.openingHours.saturday
+								: 'Stengt'}
+						</p>
+						<p>
+							{' '}
+							Søndag:{' '}
+							{store?.openingHours.sunday
+								? store.openingHours.sunday
+								: 'Stengt'}
+						</p>
+					</div>
+					<a href={store?.website}> {store?.website} </a>
+				</div>
+			</div>
+			<StandardButton
+				text={'→'}
+				state={false}
+				handleClick={() => handleStoreChange(true)}
+			/>
+		</div>
 	)
 }
